a8691a317c3fb884cc1c2e9f2937a0c43fd3c8d5
llanito7361 <llanito.code.log@gmail.com>
2024-07-04 14:15:56 -0300

    skills and experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -47,3 +47,4 @@ export const Experience = () => {
     </section>
   );
 };
+ 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/img.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
