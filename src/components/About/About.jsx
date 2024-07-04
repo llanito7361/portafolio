eeed22ffca8caba35460886193aeb31dc36b5350
llanito7361 <llanito.code.log@gmail.com>
2024-07-04 12:44:14 -0300

    responsive experience

---
+++ src/components/About/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>Sobre mi</h2>
+      <div className={styles.content}>
+        {/* <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        /> */}
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Desarrollador Frontend</h3>
+              <p>
+Soy un desarrollador frontend con experiencia en la construcción de responsive.
+ </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+              Tengo experiencia desarrollando sistemas back-end rápidos y API's
+              y API
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Habilidades blandas</h3>
+              <p>
+              Soy muy comunicativo y proactivo. Mis habilidades en la comunicación y
+      mi capacidad para tomar iniciativas se han desarrollado a lo largo de mi
+      experiencia, como instructor en colonias de
+      vacaciones y en la facultad de ingeniería.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+import bebote from './img.png'
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hola, soy Emanuel</h1>
+        <p className={styles.description}>
+          Soy un desarrollador full-stack con 2 years de experiencia usando React y NodeJS.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contactame 
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
